8948f2723c2edf4e90a62b659061dc54a9e17c3f
CO-Space-Grant <cospacegrant@colorado.edu>
2026-06-04 12:05:59 -0600

    Add files via upload

---
+++ Module_2/module2.py
@@ -0,0 +1,13 @@
+#grader for module 2
+
+# Do not modify. These are the answers for the second module. I tried to hide them but clearly you found them.
+functions_dict = {
+    "Q1": {
+        "answer": "A",
+        "hint": "Reference formula in previous section."
+    },
+    "Q2": {
+        "answer": "C",
+        "hint": "Think about which device converts a higher voltage into a steady 5V output for your microcontroller."
+    }
+}
