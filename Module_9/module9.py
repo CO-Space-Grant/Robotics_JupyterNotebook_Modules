2a247775320be093e8a8a0ef56725bbccd7b92e1
CO-Space-Grant <cospacegrant@colorado.edu>
2026-06-04 12:34:43 -0600

    Add files via upload

---
+++ Module_9/module9.py
@@ -0,0 +1,13 @@
+#grader for module 9
+
+# Do not modify. These are the answers for the third module. I tried to hide them but clearly you found them.
+functions_dict = {
+    "Q1": {
+        "answer": "B",
+        "hint": "Auto-save removes the step of remembering to save your work yourself."
+    },
+    "Q2": {
+        "answer": "C",
+        "hint": "What is simpler, two people each wanting a separate piece of pie or two people disagreeing over the same piece of pie?"
+    },
+}
