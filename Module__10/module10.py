4fab64bf2494cba06e10b9cc3f692467d7ab5d30
CO-Space-Grant <cospacegrant@colorado.edu>
2026-06-04 12:37:35 -0600

    Add files via upload

---
+++ Module__10/module10.py
@@ -0,0 +1,17 @@
+#grader for module 9
+
+# Do not modify. These are the answers for the third module. I tried to hide them but clearly you found them.
+functions_dict = {
+    "Q1": {
+        "answer": "C",
+        "hint": "Think about which of these use a camera lens first."
+    },
+    "Q2": {
+        "answer": "B",
+        "hint": "This method would factor in distance between two points to triangulate distance."
+    },
+    "Q3": {
+        "answer": "B",
+        "hint": "Which can not be traversed by our rover?"
+    }
+}
