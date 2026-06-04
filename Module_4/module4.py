9790f2501eeafb0e07a3529be9bdc62ad1c76855
CO-Space-Grant <cospacegrant@colorado.edu>
2026-06-04 12:21:07 -0600

    Add files via upload

---
+++ Module_4/module4.py
@@ -0,0 +1,30 @@
+#grader for module 4
+
+# Do not modify. These are the answers for the fourth module. I tried to hide them but clearly you found them.
+functions_dict = {
+    "Q1": {
+        "answer": "C",
+        "hint": "Think about the relationship between the signal's travel time and the distance to the object."
+    },
+    "Q2a": {
+        "answer": "D",
+        "hint": "LM393 Comparator is a component of an IR sensor."
+    },
+    "Q2b": {
+        "answer": "C",
+        "hint": "A Finite State Machine simplifies complex decision-making processes by breaking them into manageable parts."
+    },
+    "Q2c": {
+        "answer": "A",
+        "hint": "Think about how the robot keeps track of wheel movement."
+    },
+    "Q3": {
+        "answer": "C",
+        "hint": "Think about what the Echo pin does and how the function uses the time of flight (ToF) principle to calculate distance."
+    },
+    "Q4": {
+        "answer": "B",
+        "hint": "Think about how the FSM determines which actions to execute based on the current state."
+    },
+    
+}
