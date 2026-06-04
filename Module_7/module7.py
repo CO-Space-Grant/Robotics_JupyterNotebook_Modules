2ba8d5e4f36a6a1b87c461569f180387e2b6882d
CO-Space-Grant <cospacegrant@colorado.edu>
2026-06-04 12:30:41 -0600

    Add files via upload

---
+++ Module_7/module7.py
@@ -0,0 +1,21 @@
+#grader for module 7
+
+# Do not modify. These are the answers for the third module. I tried to hide them but clearly you found them.
+functions_dict = {
+    "Q1": {
+        "answer": "B",
+        "hint": "Think about how cruise control in a car works. It constantly checks the car's speed and adjusts the throttle to keep it steady. Would an open-loop system be able to do that without checking the speed?"
+    },
+    "Q2": {
+        "answer": "A",
+        "hint": "How do an IMU and a ToF sensor complement each other? One tracks movement, while the other provides absolute position data."
+    },
+    "Q3": {
+        "answer": "D",
+        "hint": "The Kalman Filter balances predictions from the IMU with real-time corrections from the ToF sensor to reduce drift and improve accuracy."
+    },
+    "Q4": {
+        "answer": "A",
+        "hint": "Wwhat happens when a sensor continuously integrates motion data without an absolute reference?"
+    },
+}
