584473c649996291b628cd3409962611456f0aa5
CO-Space-Grant <cospacegrant@colorado.edu>
2026-06-04 12:08:57 -0600

    Add files via upload

---
+++ Module_3/module3.py
@@ -0,0 +1,45 @@
+#grader for module 3
+
+# Do not modify. These are the answers for the third module. I tried to hide them but clearly you found them.
+functions_dict = {
+    "Q1": {
+        "answer": "C",
+        "hint": "Think about how layers simplify the process by dividing it into manageable steps, like turning pins on/off and controlling voltage."
+    },
+    "Q2": {
+        "answer": "A",
+        "hint": "Think about the relationship between voltage and motor speed."
+    },
+    "Q3": {
+        "answer": "A",
+        "hint": "Consider how reversing polarity affects the flow of current through the motor."
+    },
+    "Q4": {
+        "answer": "B",
+        "hint": "Think about how the duty cycle determines the proportion of time the voltage is applied."
+    },
+    "Q5": {
+        "answer": "C",
+        "hint": "Think of the motor driver as a switch that controls when the voltage reaches the motor."
+    },
+    "Q6": {
+        "answer": "B",
+        "hint": "Think about how the H-bridge uses transistors to control the voltage polarity across the motor terminals."
+    },
+    "Q7": {
+        "answer": "D",
+        "hint": "Think about how global variables allow multiple functions to access the same information."
+    },
+    "Q8": {
+        "answer": "B",
+        "hint": "Think about how OUTPUT mode enables the Arduino to control the state of the pins."
+    },
+    "Q9": {
+        "answer": "C",
+        "hint": "Analyze how the linear velocity (\( v \)) and angular velocity (\( w \)) interact for each motion. Negative values do what for \( v \)? Negative values do what for \( w \)"
+    },
+    "Q10": {
+        "answer": "B",
+        "hint": "The number inside `delay()` is in milliseconds (ms). 1000 ms = 1 second."
+    }
+}
